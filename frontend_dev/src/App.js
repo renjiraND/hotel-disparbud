e7114883b9981fef08fb7e96877373b9f9448e03
Faza Fahleraz <ffahleraz@gmail.com>
2019-03-31 21:51:35 +0700

    Init basic frontend development server

---
+++ frontend_dev/src/App.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route path="/browse" component={Browse} />
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      Welcome to Disparbud Hotel Data Center!
+    </div>
+  );
+}
+
+function Hotel({ match }) {
+  return <h3>Requested hotel id: {match.params.id}</h3>;
+}
+
+function Browse({ match }) {
+  return (
+    <div>
+      <h2>Browse Hotels</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/hotels/0`}>Hotel 0</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/hotels/1`}>Hotel 1</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/hotels/2`}>Hotel 2</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/hotels/:id`} component={Hotel} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a hotel.</h3>}
+      />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/browse">Browse</Link>
+      </li>
+    </ul>
+  );
+}
+
+export default App;
