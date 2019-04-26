80384c0d7fe2d4bff3e2973662b01d7060b3a215
RenjiraND <13516014@std.stei.itb.ac.id>
2019-04-26 07:52:37 +0700

    added detail route

--- frontend_dev/src/Routes.js
+++ frontend_dev/src/Routes.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import Dashboard from "./containers/Dashboard";
 import Input from "./containers/Input";
 import Login from "./containers/Login";
+import Detail from "./containers/Detail";
 
 export default () =>
   <Switch>
     <Route exact path="/" component={Dashboard} />
     <Route path="/input" component={Input} />
     <Route path="/login" component={Login} />
+    <Route path="/detail" component={Detail} />
   </Switch>;
