0d33a8f5a3e3f25f0d5b1d34d537dcf175e08b05
Ahmad Izzan <aahmadizzan@gmail.com>
2019-05-15 02:03:29 +0700

    [be] fix user admin page

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Hotel
-# Register your models here.
-class UserAdminPage(UserAdmin):
-    pass
-admin.site.register(User, UserAdminPage)
+from .models import Hotel # User, Hotel
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserChangeForm
+
+User = get_user_model()
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ('name', 'is_lsu',)}),
+    )
+
+
+admin.site.register(User, MyUserAdmin)
+
 admin.site.register(Hotel)
