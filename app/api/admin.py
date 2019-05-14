fe6be6f80a6ea2a6530b1f7ea062b32465e82cc7
diwangs <27280516+diwangs@users.noreply.github.com>
2019-05-14 19:51:37 +0700

    Refined admin page

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Hotel
 # Register your models here.
-admin.site.register(User)
+class UserAdminPage(UserAdmin):
+    pass
+admin.site.register(User, UserAdminPage)
 admin.site.register(Hotel)
